9085d9f343a6c3612a2184d26aefeff74628d21e
Danial Sobri <ahmaddanialbin.ahmadsobri@hs-augsburg.de>
2022-06-16 15:40:35 +0200

    01:Websocket Integration, Session Management

--- lib/server.ts
+++ lib/server.ts
@@ -2,18 +2,37 @@ import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
-import routes from './routes'
+import routes from './basic/routes';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import rkwebsocket from './rkwebsocket'
+import ip from 'ip';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
 
+/*
+ * Management of sessions
+ */
+const oneDay = 1000 * 60 * 60 * 24
+const sessionHandler = session({
+  secret: process.env.SECRET_KEY ||'thisismysecrctekeyfhrgfgrfrty84fwir767',
+  saveUninitialized: true,
+  cookie: { maxAge: oneDay },
+  resave: false
+})
+app.use(sessionHandler)
+app.use(cookieParser())
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.listen(PORT, () => {
-  console.log(`Running on ${PORT} ⚡`);
+  console.log(`Running Restful on http://${ip.address()}:${PORT} ⚡`);
   routes(app);
+  rkwebsocket(app,sessionHandler);
+
 });
